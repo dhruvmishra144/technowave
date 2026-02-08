44becdaa87aa5652a65c76b43b912af49b74974c
Manoj Kushwaha <technowave108@gmail.com>
2026-02-08 18:40:10 +0000

    this error is causing by tawk, just silece it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,19 +55,6 @@ export default function RootLayout({
         </div>
         <Footer />
         <Toaster />
-        <Script id="tawk-to-script" strategy="afterInteractive">
-          {`
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/6988b6f83202f91c36de08b5/1jgv0lfnj';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-              })();
-          `}
-        </Script>
       </body>
     </html>
   );
