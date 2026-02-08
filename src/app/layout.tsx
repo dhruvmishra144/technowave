e645cb9ffae3a8f1b6a3d8aa1d9b9e7ac22f4644
Manoj Kushwaha <technowave108@gmail.com>
2026-02-08 18:34:47 +0000

    fix dangerouslySetInnerHTML in layout wth best practice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
@@ -30,18 +31,16 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <Script id="touch-detector" strategy="beforeInteractive">
+          {`
               const isTouchDevice = 'ontouchstart' in window;
               if (isTouchDevice) {
                 document.documentElement.classList.add('touchevents');
               } else {
                 document.documentElement.classList.remove('touchevents');
               }
-            `,
-          }}
-        />
+          `}
+        </Script>
       </head>
       <body
         className={cn(
@@ -56,10 +55,8 @@ export default function RootLayout({
         </div>
         <Footer />
         <Toaster />
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
+        <Script id="tawk-to-script" strategy="afterInteractive">
+          {`
               var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
               (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -69,9 +66,8 @@ export default function RootLayout({
               s1.setAttribute('crossorigin','*');
               s0.parentNode.insertBefore(s1,s0);
               })();
-            `,
-          }}
-        />
+          `}
+        </Script>
       </body>
     </html>
   );
